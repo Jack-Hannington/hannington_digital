cd263038386f2f09c8f3e76cf2d92774124d8116
jack hannington <hannington94@gmail.com>
2022-12-16 21:33:20 +0000

    sections update

--- public/index.js
+++ public/index.js
@@ -5,3 +5,16 @@ window.onscroll = function() {
         document.querySelector('header').classList.remove('scrolled');
       }
   };
+
+
+//  window.onload() document.querySelector('#show-menu').addEventListener('mouseover', () =>{
+//         alert('mouse')
+//   })
+
+
+  window.addEventListener("load", (event) => {
+    document.querySelector('#show-menu').addEventListener('click', () =>{
+        alert('still-working')
+        // document.querySelector('.menu').classList.add('show-menu');
+  })
+  });
