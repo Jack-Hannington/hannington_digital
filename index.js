6458fe1a79b720d81691ae91af8b73315b76f127
jack hannington <hannington94@gmail.com>
2022-12-17 13:36:56 +0000

    updates

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 50) {
-        document.querySelector('header').classList.add('scrolled');
-      } else {
-        document.querySelector('header').classList.remove('scrolled');
-      }
-  };
+// window.onscroll = function() {
+//     if (document.documentElement.scrollTop > 50) {
+//         document.querySelector('header').classList.add('scrolled');
+//       } else {
+//         document.querySelector('header').classList.remove('scrolled');
+//       }
+//   };
 
 
-//  window.onload() document.querySelector('#show-menu').addEventListener('mouseover', () =>{
-//         alert('mouse')
-//   })
+// //  window.onload() document.querySelector('#show-menu').addEventListener('mouseover', () =>{
+// //         alert('mouse')
+// //   })
 
 
-  window.addEventListener("DOMContentLoaded", (event) => {
-      function updateMenu(){
-        document.querySelector('#show-menu').addEventListener('click', () =>{
-               let menu = document.querySelector('.menu');
-               menu.classList.toggle('show-menu');
-         })
-      }
-      setTimeout(updateMenu,1000)
+//   window.addEventListener("DOMContentLoaded", (event) => {
+//       function updateMenu(){
+//         document.querySelector('#show-menu').addEventListener('click', () =>{
+//                let menu = document.querySelector('.menu');
+//                menu.classList.toggle('show-menu');
+//          })
+//       }
+//       setTimeout(updateMenu,1000)
 
-  });
+//   });
