5ee927066fba8afe43b982d578ce9b28d44a1124
jack hannington <hannington94@gmail.com>
2023-01-14 19:15:28 +0000

    updated shadows for weight selections

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const userWeight = document.querySelector('#user-weight');
 const intensity = document.querySelector('input[type="range"]');
 const allMacros = document.querySelectorAll('.macros');
-const total = document.querySelector('#total');
+const totalGrams = document.querySelector('#total-grams');
+const totalCals = document.querySelector('#total-kcals');
 const loseWeight = document.querySelector('#lose-weight');
 const gainWeight = document.querySelector('#gain-weight');
 const maintainWeight = document.querySelector('#maintain-weight');
+const macrosCard = document.querySelector('.macros-card');
 intensity.disabled = true;
 const macroData = [
     { id: "protein", kcals: 4, baseline: 1 },
@@ -29,29 +31,40 @@ function updateMacros() {
             allMacros[i].textContent = parseInt((macroData[i].baseline + (intensity.value / 10 + 0.1)) * userWeight.value)
             totalMacros.push(parseInt((macroData[i].baseline + (intensity.value / 10 + 0.1)) * userWeight.value))
             const sum = totalMacros.reduce((partialSum, a) => partialSum + a, 0);
+         
+            totalGrams.textContent = (sum);
 
-            total.textContent = (sum);
         }
     }
+
     rangeValue = intensity.value;
     if (rangeValue >= 1 ){
         loseWeight.style.opacity = 0.3;
         gainWeight.style.opacity = 1;
         maintainWeight.style.opacity = 0.5;
+        macrosCard.classList.remove('blue-shadow')
+        macrosCard.classList.add('red-shadow')
+        
     } else if (rangeValue == 0 ){
         loseWeight.style.opacity = 0.5;
         gainWeight.style.opacity = 0.5;
         maintainWeight.style.opacity = 1;
+        macrosCard.classList.remove('red-shadow');
+        macrosCard.classList.remove('blue-shadow')
     } else {
         loseWeight.style.opacity = 1;
         gainWeight.style.opacity = 0.3;
         maintainWeight.style.opacity = 0.5;
+        macrosCard.classList.remove('red-shadow')
+        macrosCard.classList.add('blue-shadow')
+    
     }
     const totalKcals = totalMacros.map(macro => macro * 4);
+    document.querySelector('#protein-kcal').textContent = totalKcals[0]
+    document.querySelector('#carbs-kcal').textContent = totalKcals[1]
+    document.querySelector('#fats-kcal').textContent = totalKcals[2]
     const sum = totalKcals.reduce((partialSum, a) => partialSum + a, 0);
-    let totals = document.createElement('p')
-    totals.textContent = (`Kcals: ${sum}`);
-    total.appendChild(totals)
+    totalCals.textContent = sum; 
 }
 
 intensity.addEventListener('change', updateMacros);
